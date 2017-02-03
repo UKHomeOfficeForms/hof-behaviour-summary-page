aa6065f2f563b1804e008b19c1d906bc26fd9fc4
Leonard Martin <leonard.martin@gmail.com>
2017-02-03 16:51:49 +0000

    MOAR TESTS!

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -2,12 +2,16 @@
 
 const reqres = require('reqres');
 const Model = require('hof-model');
+const chai = require('chai');
 
-global.expect = require('chai').expect;
+global.expect = chai.expect;
+global.sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
 
 global.request = (obj) => {
   return reqres.req(Object.assign({
-    translate: (a) => a,
+    translate: sinon.spy((a) => Array.isArray(a) ? a[0] : a),
     sessionModel: new Model()
   }, obj));
 };
